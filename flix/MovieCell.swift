4b697521bca3b8ad5091448a56bdd1ad7d985951
Oscar Reyes <oxreyes@ucsd.edu>
2017-12-21 20:36:11 -0800

    Done

---
+++ flix/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  flix
+//
+//  Created by Oscar Reyes on 12/21/17.
+//  Copyright © 2017 Oscar Reyes. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
