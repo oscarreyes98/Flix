30dfb3a6369eec3c6c00bc126d5d04d3eb742e42
Oscar Reyes <oxreyes@ucsd.edu>
2017-12-26 21:08:55 -0800

    done

---
+++ flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix
+//
+//  Created by Oscar Reyes on 12/26/17.
+//  Copyright © 2017 Oscar Reyes. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
